dc64e422d906c6a78bda5e0f27b5327c82eab2ef
Shin Gunsu <gunsu980@gmail.com>
2024-04-14 02:20:45 +0900

    오타수정

--- test/model_intent_test.py
+++ test/model_intent_test.py
@@ -7,10 +7,10 @@ from utils.Preprocess import Preprocess
 from models.intent.IntentModel import IntentModel
 
 p = Preprocess(word2index_dic=file_path+'/../train_tools/dict/chatbot_dict.bin',
-               userdic=file_path + '/../utils/user_dic.txt')
+               userdic=file_path + '/../utils/user_dict.txt')
 
 intent = IntentModel(model_name=file_path + '/../models/intent/intent_model.h5', proprocess=p)
-query = "오늘 탕수육 주문 가능한가요?"
+query = ""
 predict = intent.predict_class(query)
 predict_label = intent.labels[predict]
 
