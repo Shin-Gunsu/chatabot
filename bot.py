abdfdf20397bd94acf1573166a520e434ed83a02
guns <guns>
2024-05-08 10:48:21 +0900

    modify GetAnswer

--- bot.py
+++ bot.py
@@ -76,11 +76,13 @@ def to_client(conn, addr):
 
             #ASSISTANT 모델
             #thread id는 유저 생길때 마다 새로 부여해야함,유저 종료시 스레드 삭제
-            thread_id = ""
+            thread_id = "thread_1CFWA8SZL27chvsuQXGcOkLX"
             assistant_model = GetAnswer_assistant(OpenAI(api_key=gptapi_key),thread_id)
+            
             answer = assistant_model.ask(query)
+            print(answer)
             send_json_data_str = {
-                "Query" : answer,
+                "Answer" : answer,
                 "Intent": intent_predict[1],
                 "Ner": ner_list
             }
