92cd63c5322051bf93309c6e28f339016dab8a93
Shin Gunsu <gunsu980@gmail.com>
2024-05-10 15:30:18 +0900

    의도분류모델 데이터 재생성

--- models/test/make_que.py
+++ models/test/make_que.py
@@ -4,23 +4,23 @@ import os
 
 file_path = os.path.dirname(__file__) 
 word_file = file_path + '/keyword.csv'
-word_file2=file_path + '/keyword2.csv'
-sent_file = file_path + '/주문조합.csv'
+word_file2 =file_path + '/keyword2.csv'
+word_file3 = file_path + '/keyword3.csv'
 
-output_file = file_path + '/train_data.csv'
+output_file = file_path + '/degree_requirement.csv'
 
 with open(output_file, mode='w',newline='', encoding='utf-8') as f_out:
-    with open(word_file2, mode='r', encoding='utf-8') as f2:
-        reader2 = csv.reader(f2)
-        for i2, row2 in enumerate(reader2):
+    with open(word_file, mode='r', encoding='utf-8') as f1:
+        reader1 = csv.reader(f1)
+        for i2, row2 in enumerate(reader1):
             word2 = str(row2[0])
-            with open(word_file, mode='r', encoding='utf-8') as f:
-                reader = csv.reader(f)
-                for i, row in enumerate(reader):
+            with open(word_file2, mode='r', encoding='utf-8') as f2:
+                reader2 = csv.reader(f2)
+                for i, row in enumerate(reader2):
                     word = word2 +' '+ str(row[0])
 
-                    with open(sent_file, mode="r", encoding="utf-8") as qf:
+                    with open(word_file3, mode="r", encoding="utf-8") as qf:
                         qreader = csv.reader(qf)
                         for j, qrow in enumerate(qreader):
-                            q = word + ' ' + qrow[0] + ',4\n'
+                            q = word + ' ' + qrow[0] + ',0\n'
                             f_out.write(q)
--- utils/FindAnswer.py
+++ utils/FindAnswer.py
@@ -1,33 +1,10 @@
 class FindAnswer:
     def __init__(self) -> None:
         pass
-    # def __init__(self, db):
-    #     self.db = db
-
-    # 검색 쿼리 생성
-    # def _make_query(self, intent_name, ner_tags):
-        # sql = "select * from chatbot_train_data"
-        # if intent_name != None and ner_tags == None:
-        #     sql = sql + " where intent='{}' ".format(intent_name)
-
-        # elif intent_name != None and ner_tags != None:
-        #     where = ' where intent="%s" ' % intent_name
-        #     if (len(ner_tags) > 0):
-        #         where += 'and ('
-        #         for ne in ner_tags:
-        #             where += " ner like '%{}%' or ".format(ne)
-        #         where = where[:-3] + ')'
-        #     sql = sql + where
-
-        # # 동일한 답변이 2개 이상인 경우, 랜덤으로 선택
-        # sql = sql + " order by rand() limit 1"
-        # return sql
-
-
 
     # 답변 검색
     def search(self, intent_name, ner_tags):
-        answer = ['청솔관은 여기','이미지없음']
+        answer = ['답변출력','이미지출력']
         return (answer[0], answer[1])
 
     # NER 태그를 실제 입력된 단어로 변환
